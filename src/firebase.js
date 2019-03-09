a107b78c282aac935dc6a7a62e481aa18b533dfd
Nellineo <georgenellas@hotmail.com>
2019-03-09 19:43:16 +0200

    firebase bug fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 
 const config = {
     apiKey: "AIzaSyCtzL4tlZUSPZHzPb_JFWa3s8SWhUIGbkQ",
