fc83e889758fc02c5000488956fa08587718825c
Nellineo <georgenellas@hotmail.com>
2019-03-12 21:34:00 +0200

    Added Public/Private Routes + Admin Dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+
 import Home from './Components/home';
 import SignIn from './Components/sign_in';
 
+import Dashboard from './Components/admin/Dashboard';
+
 const Routes = (props) => {
+
+
   return (
     <Layout>
       <Switch>
-        <Route exact path="/sign_in" component={SignIn} />
-        <Route exact path="/" component={Home} />
+
+        <PrivateRoute {...props} exact path="/dashboard" component={Dashboard}/>
+        <PublicRoute {...props} restricted={true}  exact path="/" component={Home}/>
+        <PublicRoute {...props} restricted={true} exact path="/sign_in" component={SignIn}/>
       </Switch>
     </Layout>
   )
